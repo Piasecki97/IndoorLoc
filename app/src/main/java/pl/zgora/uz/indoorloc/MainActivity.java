a8b8ad981d514af82c0778599eed854f176f91d9
Damian Piasecki <piasecki9712@gmail.com>
2023-03-30 21:04:44 +0200

    Sync changes

--- app/src/main/java/pl/zgora/uz/indoorloc/MainActivity.java
+++ app/src/main/java/pl/zgora/uz/indoorloc/MainActivity.java
@@ -1,108 +1,33 @@
 package pl.zgora.uz.indoorloc;
 
-import android.Manifest;
-import android.app.Activity;
-import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.content.pm.PackageManager;
-import android.os.Build;
 import android.os.Bundle;
-import android.view.Menu;
-import android.widget.LinearLayout;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-import pl.zgora.uz.indoorloc.databinding.ActivityMainBinding;
-
 
 public class MainActivity extends AppCompatActivity {
-    private ActivityMainBinding binding;
-    private BluetoothAdapter btAdapter = BluetoothAdapter.getDefaultAdapter();
-
-    private LinearLayout linearLayout;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        System.out.println("init");
-
-        registerReceiver(receiver, new IntentFilter(BluetoothDevice.ACTION_FOUND));
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        linearLayout = findViewById(R.id.linear_container);
-        setContentView(binding.getRoot());
-
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(binding.navView, navController);
-        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.BLUETOOTH_CONNECT) == PackageManager.PERMISSION_DENIED)
-        {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S)
-            {
-                ActivityCompat.requestPermissions(MainActivity.this, new String[]{android.Manifest.permission.BLUETOOTH_CONNECT}, 2);
-            }
-        }
-
-        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.BLUETOOTH_SCAN) == PackageManager.PERMISSION_DENIED)
-        {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S)
-            {
-                ActivityCompat.requestPermissions(MainActivity.this, new String[]{android.Manifest.permission.BLUETOOTH_SCAN}, 2);
-            }
-        }
-
-        btAdapter.startDiscovery();
-        System.out.println("start");
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.bottom_nav_menu, menu);
-        return true;
-    }
-
-    private final BroadcastReceiver receiver = new BroadcastReceiver(){
-        @Override
+    private final BroadcastReceiver receiver = new BroadcastReceiver() {
         public void onReceive(Context context, Intent intent) {
-
             String action = intent.getAction();
-            if(BluetoothDevice.ACTION_FOUND.equals(action)) {
-                short rssi = intent.getShortExtra(BluetoothDevice.EXTRA_RSSI,Short.MIN_VALUE);
-                String name = intent.getStringExtra(BluetoothDevice.EXTRA_NAME);
-                LinearLayout linearLayoutHor = new LinearLayout(context);
-                linearLayoutHor.setOrientation(LinearLayout.HORIZONTAL);
-                TextView btDeviceNameArea = new TextView(context);
-                TextView rssiArea = new TextView(context);
-                if(name != null) {
-                    btDeviceNameArea.setText(name);
-                    rssiArea.setText(rssi);
-                    linearLayoutHor.addView(btDeviceNameArea);
-                    linearLayoutHor.addView(rssiArea);
-                    linearLayout.addView(linearLayoutHor);
-                }
-            } else {
-                Toast.makeText(context, "Failed to find any devices", Toast.LENGTH_SHORT);
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                // Discovery has found a device. Get the BluetoothDevice
+                // object and its info from the Intent.
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                String deviceName = device.getName();
+                String deviceHardwareAddress = device.getAddress(); // MAC address
             }
         }
     };
-
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // Register for broadcasts when a device is discovered.
+        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        registerReceiver(receiver, filter);
+    }
 }
